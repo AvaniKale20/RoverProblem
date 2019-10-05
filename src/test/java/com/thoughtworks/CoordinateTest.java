0d7a3adbffad5ed6a79c7f0eb4c83fa5ba140884
avanijayantkale <Avanikale20>
2019-10-05 19:59:17 +0530

    Add. can check given coordinate is upper or not

--- src/test/java/com/thoughtworks/CoordinateTest.java
+++ src/test/java/com/thoughtworks/CoordinateTest.java
@@ -8,21 +8,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CoordinateTest {
 
     @Test
-    void givenCoordinate_WhenCheckIsSameOrNot_ThenShouldReturnTrue()
-    {
-        Coordinate coordinate=new Coordinate(0,0);
-        assertTrue(coordinate.isSame(0,0));
+    void givenCoordinate_WhenCheckIsSameOrNot_ThenShouldReturnTrue() {
+        Coordinate coordinate = new Coordinate(0, 0);
+        assertTrue(coordinate.isSame(0, 0));
     }
+
     @Test
-    void givenOneCoordinateAndOneCoordinate_WhenCheckIsSameOrNot_ThenShouldReturnTrue()
-    {
-        Coordinate coordinate=new Coordinate(1,1);
-        assertTrue(coordinate.isSame(1,1));
+    void givenOneCoordinateAndOneCoordinate_WhenCheckIsSameOrNot_ThenShouldReturnTrue() {
+        Coordinate coordinate = new Coordinate(1, 1);
+        assertTrue(coordinate.isSame(1, 1));
     }
+
     @Test
-    void givenZeroCoordinateAndZeroCoordinate_WhenCheckIsUpperOrNot_ThenShouldReturnTrue()
-    {
-        Coordinate coordinate=new Coordinate(0,0);
-        assertFalse(coordinate.isUpper(1,1));
+    void givenZeroCoordinateAndZeroCoordinate_WhenCheckIsUpperOrNot_ThenShouldReturnTrue() {
+        Coordinate coordinate = new Coordinate(0, 0);
+        assertFalse(coordinate.isUpper(1, 1));
+    }
+
+    @Test
+    void givenOneCoordinateAndOneCoordinate_WhenCheckIsUpperOrNot_ThenShouldReturnTrue() {
+        Coordinate coordinate = new Coordinate(1, 1);
+        assertTrue(coordinate.isUpper(0, 0));
     }
 }
