6a201545885f55edc48c53b981c8635b01fa4bbd
avanijayantkale <Avanikale20>
2019-10-05 19:47:38 +0530

    Add. git ignore file

--- src/test/java/com/thoughtworks/CoordinateTest.java
+++ src/test/java/com/thoughtworks/CoordinateTest.java
@@ -11,5 +11,11 @@ public class CoordinateTest {
         Coordinate coordinate=new Coordinate(0,0);
         Assertions.assertTrue(coordinate.isSame(0,0));
     }
+    @Test
+    void givenOneCoordinateAndOneCoordinate_WhenCheckIsSameOrNot_ThenShouldReturnTrue()
+    {
+        Coordinate coordinate=new Coordinate(1,1);
+        Assertions.assertTrue(coordinate.isSame(1,1));
+    }
 
 }
