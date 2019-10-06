2ac4acf6525f4638371521a174a1fc07ea1303e9
avanijayantkale <Avanikale20>
2019-10-06 13:31:46 +0530

    Add. can check negative one and negative one coordinate is bellow from origin

--- src/test/java/com/thoughtworks/CoordinateTest.java
+++ src/test/java/com/thoughtworks/CoordinateTest.java
@@ -12,4 +12,15 @@ public class CoordinateTest {
         assertTrue(coordinate.isAbove(new Coordinate(0, 0)));
     }
 
+    @Test
+    void givenTwoAndOneCoordinate_WhenCheckIsAboveFromOriginalPosition_ThenReturnTrue() {
+        Coordinate coordinate = new Coordinate(2, 2);
+        assertTrue(coordinate.isAbove(new Coordinate(0, 0)));
+    }
+
+    @Test
+    void givenNegativeOneAndOneCoordinate_WhenCheckIsBellowFromOriginalPosition_ThenReturnTrue() {
+        Coordinate coordinate = new Coordinate(-1, -1);
+        assertTrue(coordinate.isBellow(new Coordinate(0, 0)));
+    }
 }
