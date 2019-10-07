8cf4dd8d12dce1357da83f4ed5d20583fa80e687
avanijayantkale <Avanikale20>
2019-10-07 14:29:19 +0530

    Add. can check one and one coordinate is right

--- src/test/java/com/thoughtworks/CoordinateTest.java
+++ src/test/java/com/thoughtworks/CoordinateTest.java
@@ -3,12 +3,20 @@ package com.thoughtworks;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CoordinateTest {
 
     @Test
     void givenOneAndOneCoordinate_WhenCheckIsRight_ThenReturnFalse() {
         Coordinate coordinate = new Coordinate(0, 0);
-        Assertions.assertFalse(coordinate.isRight(new Coordinate(1, 1)));
+        assertFalse(coordinate.isRight(new Coordinate(1, 1)));
+    }
+
+    @Test
+    void givenOneAndOneCoordinate_WhenCheckIsRight_ThenReturnTrue() {
+        Coordinate coordinate = new Coordinate(1, 1);
+        assertTrue(coordinate.isRight(new Coordinate(0, 0)));
     }
 
 
