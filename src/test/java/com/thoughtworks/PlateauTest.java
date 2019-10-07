3fc96a4e9cf3cff85a2905347bd9cdc430b75493
avanijayantkale <Avanikale20>
2019-10-07 13:22:16 +0530

    Refactor. Redundant code removed from codebase

--- src/test/java/com/thoughtworks/PlateauTest.java
+++ src/test/java/com/thoughtworks/PlateauTest.java
@@ -1,40 +1,41 @@
 package com.thoughtworks;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlateauTest {
 
+    private Coordinate getOrigin() {
+        return new Coordinate(0, 0);
+    }
+
+    private Coordinate getEnd() {
+        return new Coordinate(4, 4);
+    }
+
     @Test
     void givenRover_whenCheckIsWithinPlateau_ThenReturnTrue() {
-        Coordinate origin = new Coordinate(0, 0);
-        Coordinate end = new Coordinate(4, 4);
-        Plateau plateau = new Plateau(origin, end);
+        Plateau plateau = new Plateau(getOrigin(), getEnd());
         assertTrue(plateau.hasWithingBound(new Coordinate(0, 1)));
     }
 
     @Test
     void givenRoverWithZeroXCoordinateAndNegativeOnYCoordinate_whenCheckIsWithinPlateau_ThenReturnFalse() {
-        Coordinate origin = new Coordinate(0, 0);
-        Coordinate end = new Coordinate(4, 4);
-        Plateau plateau = new Plateau(origin, end);
+        Plateau plateau = new Plateau(getOrigin(), getEnd());
         assertFalse(plateau.hasWithingBound(new Coordinate(0, -1)));
     }
 
     @Test
     void givenRoverWithOneXCoordinateAndZeroYCoordinate_whenCheckIsWithinPlateau_ThenReturnTrue() {
-        Coordinate origin = new Coordinate(0, 0);
-        Coordinate end = new Coordinate(4, 4);
-        Plateau plateau = new Plateau(origin, end);
+        Plateau plateau = new Plateau(getOrigin(), getEnd());
         assertTrue(plateau.hasWithingBound(new Coordinate(1, 0)));
     }
 
     @Test
     void givenRoverWithNegativeOneXCoordinateAndZeroYCoordinate_whenCheckIsWithinPlateau_ThenReturnFalse() {
-        Coordinate origin = new Coordinate(0, 0);
-        Coordinate end = new Coordinate(4, 4);
-        Plateau plateau = new Plateau(origin, end);
+        Plateau plateau = new Plateau(getOrigin(), getEnd());
         assertFalse(plateau.hasWithingBound(new Coordinate(-1, 0)));
     }
 }
